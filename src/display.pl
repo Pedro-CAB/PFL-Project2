91aa453069074934a1e4ffe18099e6e136701aa0
Mariana Teixeira <76732179+marianalt@users.noreply.github.com>
2022-12-22 20:28:42 +0000

    [FIX] Recursion bug. [ADD] Movement of pieces.

--- src/display.pl
+++ src/display.pl
@@ -1,7 +1,5 @@
 %Funções que fazem display do tabuleiro e de elementos complexos do jogo
 
-showErrorMessage :- write('ERROR').
-
 showBoard(B,1) :- % B -> Lista que representa o Tabuleiro.
            write('             Column       \n'),
            write('       |1|2|3|4|5|6|7|8|9|\n'),
--- src/main.pl
+++ src/main.pl
@@ -1,5 +1,6 @@
 :- consult(display).
 :- consult(analyze).
+:- consult(utils).
 
 play :-
            write('Welcome to Wana!\n'),
@@ -25,33 +26,51 @@ start(M) :-
 
 game(M,B) :-
            M = a,
-           turn(1,B),
+           turn(1,B,N),
            %not(win(B)), win/lose shows board and result (who won)
-           turn(2,B),
+           turn(2,N,R),
            %not(win(B)),
-           game(M,B).
+           game(M,R).
 
-turn(P,B) :-
+turn(P,B,N) :-
            write('Player '), write(P), write(' turn...\n'),
            showBoard(B,1),
-           moveChoice(B,P).
+           moveChoice(B,P,N).
                 
-moveChoice(B,P) :-
+moveChoice(B,P,N) :-
            write('Which piece do you want to move?\n'),
            write('Insert the line of the piece:\n'),
            read(L1),
            write('Insert the column of the piece:\n'),
            read(C1),
-           (\+isPlayerPiece(L1,C1,B,P) -> pieceChoiceMessage(P), showBoard(B,1), moveChoice(B,P),!; isPlayerPiece(L1,C1,B,P)),
-           write('Where do you want to move it to?\n'),
-           write('Insert the line of the position:\n'),
-           read(L2),
-           write('Insert the column of the position:\n'),
-           read(C2),
-           (\+isFree(L2,C2,B) -> tryMoveAgainMessage, showBoard(B,1), moveChoice(B,P), !; isFree(L2,C2,B)),
-           write('Should move the piece from '), write(L1),write('-'),write(C1), write(' to '),write(L2),write('-'),write(C2), write('\n'). %TODO - move.
+           (\+isPlayerPiece(L1,C1,B,P) -> pieceChoiceMessage(P), showBoard(B,1), moveChoice(B,P,N);
+                isPlayerPiece(L1,C1,B,P),
+                write('Where do you want to move it to?\n'),
+                write('Insert the line of the position:\n'),
+                read(L2),
+                write('Insert the column of the position:\n'),
+                read(C2),
+                (\+isFree(L2,C2,B) -> tryMoveAgainMessage, showBoard(B,1), moveChoice(B,P,N), !;
+                        isFree(L2,C2,B),
+                        %write('Should move the piece from '), write(L1),write('-'),write(C1), write(' to '),write(L2),write('-'),write(C2), write('\n')
+                        movePiece(L1,C1,L2,C2,B,N)
+                )
+           ).
 
 pieceChoiceMessage(P) :- write('There isn\'t any pieces of yours there!\n'),
                 write('Try again, player '), write(P), write('!\n').
 
-tryMoveAgainMessage :- write('You can\'t place the piece there! Try choosing another piece...\n').                
+tryMoveAgainMessage :- write('You can\'t place the piece there! Try choosing another piece...\n').  
+
+movePiece(L1,C1,L2,C2,B,Result) :-
+           indexOf(L1,B,X), %X é a linha do tabuleiro da peca a mover
+           indexOf(C1,X,Y), %Y é a peça a mover
+           indexOf(L2,B,W), %W é a linha do tabuleiro da peca destino
+           indexOf(C2,W,Z), %Z é a peça destino
+           L3 is L1-1,
+           C3 is C1-1,
+           replace(B, L3 , C3 , Z , B1),
+           L4 is L2-1,
+           C4 is C2-1,
+           replace(B1, L4 , C4 , Y , Result).
+
